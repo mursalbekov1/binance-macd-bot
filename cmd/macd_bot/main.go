7aa1c936910615ea8275ec0cc7142303aaf8c110
mursalbekov1 <mursalbekov.merey@gmail.com>
2023-09-16 14:47:06 +0600

    message macd histogram

--- cmd/macd_bot/main.go
+++ cmd/macd_bot/main.go
@@ -1,80 +1,30 @@
 package main
 
 import (
-	"binance_tg/internal/functions"
-	"context"
+	"binance_tg/internal/api"
 	"fmt"
-	"github.com/adshao/go-binance/v2"
 	"log"
-	"strconv"
-	"time"
 )
 
 // binance api
-var (
-	apiKey    = "DQbMjZ54eTkw7pHIxYYW1UUFWNADxaETaE79C6Ad0VH69QImdQqVLE9rpJ6ZFc28"
-	secretKey = "Mdiyex2E6kiQf2jmOSjrXKsTbqTb4SdURt3LqumbqZi3OdNSUhP3I0NTi8dHdBDG"
-)
 
 func main() {
 
-	client := binance.NewClient(apiKey, secretKey)
-	symbol := "BTCUSDT"
-	interval := "1s"
-	limit := 100
+	//https://api.telegram.org/bot<token>/METHOD_NAME
+	botToken := "6447935217:AAFVtGASsalRTgVXXUae31OrrIYXGyYnBbQ"
+	botApi := "https://api.telegram.org/bot"
+	botUrl := botApi + botToken
+	offset := 0
 
 	for {
-		klines, err := client.NewKlinesService().
-			Symbol(symbol).
-			Interval(interval).
-			Limit(limit).
-			Do(context.Background())
+		updates, err := api.GetUpdates(botUrl, offset)
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Something went wrong: ", err.Error())
 		}
-
-		var prices []float64
-		for _, kline := range klines {
-			closePriceStr := kline.Close
-			closePrice, err := strconv.ParseFloat(closePriceStr, 64)
-			if err != nil {
-				log.Fatal(err)
-			}
-			prices = append(prices, closePrice)
+		for _, update := range updates {
+			err = api.Respond(botUrl, update)
+			offset = update.UpdateId + 1
 		}
-
-		// Периоды для вычисления MACD и сигнальной линии
-		shortPeriod := 12
-		longPeriod := 26
-		signalPeriod := 9
-
-		// Вычисляем MACD и сигнальную линию
-		macd, signalLine := functions.CalculateMACD(prices, shortPeriod, longPeriod, signalPeriod)
-
-		// Выводим последний результат MACD
-		//fmt.Println("MACD line:", macd[len(macd)-1])
-		//fmt.Println("Signal line:", signalLine[len(signalLine)-1])
-		fmt.Println("Histogram1:", macd[len(macd)-1]-signalLine[len(signalLine)-1])
-		fmt.Println("Time: ", time.Now().Format("15:04:05.000"))
-		// Задержка в 3 секунды перед следующим запросом
-		time.Sleep(3 * time.Second)
+		fmt.Println(updates)
 	}
-
-	//https://api.telegram.org/bot<token>/METHOD_NAME
-	//botToken := "6447935217:AAFVtGASsalRTgVXXUae31OrrIYXGyYnBbQ"
-	//botApi := "https://api.telegram.org/bot"
-	//botUrl := botApi + botToken
-	//offset := 0
-	//
-	//for {
-	//	updates, err := api.GetUpdates(botUrl, offset)
-	//	if err != nil {
-	//		log.Println("Something went wrong: ", err.Error())
-	//	}
-	//	for _, update := range updates {
-	//		err = api.Respond(botUrl, update)
-	//		offset = update.UpdateId + 1
-	//	}
-	//	fmt.Println(updates)
-	//}
 }
--- internal/api/respond.go
+++ internal/api/respond.go
@@ -4,13 +4,24 @@ import (
 	"binance_tg/internal/models"
 	"bytes"
 	"encoding/json"
+	"github.com/adshao/go-binance/v2"
 	"net/http"
+	"strconv"
+)
+
+var (
+	apiKey    = "DQbMjZ54eTkw7pHIxYYW1UUFWNADxaETaE79C6Ad0VH69QImdQqVLE9rpJ6ZFc28"
+	secretKey = "Mdiyex2E6kiQf2jmOSjrXKsTbqTb4SdURt3LqumbqZi3OdNSUhP3I0NTi8dHdBDG"
+	client    = binance.NewClient(apiKey, secretKey)
+	symbol    = "BTCUSDT"
+	interval  = "1s"
+	limit     = 100
 )
 
 func Respond(botUrl string, update models.Update) error {
 	var botMessage models.BotMessage
 	botMessage.ChatId = update.Message.Chat.ChatId
-	botMessage.Text = update.Message.Text
+	botMessage.Text = strconv.FormatFloat(GetMACD(client, symbol, interval, limit), 'f', -1, 64)
 	buf, err := json.Marshal(botMessage)
 	if err != nil {
 		return err
