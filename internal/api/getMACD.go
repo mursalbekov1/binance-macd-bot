6ae850060cf4e2519dc233f78c17d5ade4aecbc5
mursalbekov1 <mursalbekov.merey@gmail.com>
2023-09-17 16:41:33 +0600

    carcase of project

--- internal/api/getMACD.go
+++ internal/api/getMACD.go
@@ -11,46 +11,37 @@ import (
 
 func GetMACD(client *binance.Client, symbol string, interval string, limit int) float64 {
 
-	klines, err := client.NewKlinesService().
-		Symbol(symbol).
-		Interval(interval).
-		Limit(limit).
-		Do(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
 
-	var prices []float64
-	for _, kline := range klines {
-		closePriceStr := kline.Close
-		closePrice, err := strconv.ParseFloat(closePriceStr, 64)
+		klines, err := client.NewKlinesService().
+			Symbol(symbol).
+			Interval(interval).
+			Limit(limit).
+			Do(context.Background())
 		if err != nil {
 			log.Fatal(err)
 		}
-		prices = append(prices, closePrice)
+
+		var prices []float64
+		for _, kline := range klines {
+			closePriceStr := kline.Close
+			closePrice, err := strconv.ParseFloat(closePriceStr, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			prices = append(prices, closePrice)
+		}
+
+		// Периоды для вычисления MACD и сигнальной линии
+		shortPeriod := 12
+		longPeriod := 26
+		signalPeriod := 9
+
+		// Вычисляем MACD и сигнальную линию
+		macd, signalLine := functions.CalculateMACD(prices, shortPeriod, longPeriod, signalPeriod)
+
+		time.Sleep(time.Second)
+		return macd[len(macd)-1] - signalLine[len(signalLine)-1]
 	}
-
-	// Периоды для вычисления MACD и сигнальной линии
-	shortPeriod := 12
-	longPeriod := 26
-	signalPeriod := 9
-
-	// Вычисляем MACD и сигнальную линию
-	macd, signalLine := functions.CalculateMACD(prices, shortPeriod, longPeriod, signalPeriod)
-
-	// Выводим последний результат MACD
-	//fmt.Println("MACD line:", macd[len(macd)-1])
-	//fmt.Println("Signal line:", signalLine[len(signalLine)-1])
-	//fmt.Print("Histogram1: ", macd[len(macd)-1]-signalLine[len(signalLine)-1])
-	//if (macd[len(macd)-1] - signalLine[len(signalLine)-1]) > 0 {
-	//	println("\U0001F7E2")
-	//} else {
-	//	println("\U0001F534")
-	//}
-	//fmt.Println("Time: ", time.Now().Format("15:04:05.000"))
-	// Задержка в 3 секунды перед следующим запросом
-
-	time.Sleep(time.Second)
-	return macd[len(macd)-1] - signalLine[len(signalLine)-1]
 
 }
--- internal/api/respond.go
+++ internal/api/respond.go
@@ -5,8 +5,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/adshao/go-binance/v2"
+	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 var (
@@ -16,12 +18,31 @@ var (
 	symbol    = "BTCUSDT"
 	interval  = "1s"
 	limit     = 100
+	isRunning = false
 )
 
 func Respond(botUrl string, update models.Update) error {
 	var botMessage models.BotMessage
 	botMessage.ChatId = update.Message.Chat.ChatId
-	botMessage.Text = strconv.FormatFloat(GetMACD(client, symbol, interval, limit), 'f', -1, 64)
+	if update.Message.Text == "/start" {
+		if isRunning {
+			botMessage.Text = "MACD уже запущено."
+		} else {
+			botMessage.Text = "MACD запущено."
+			isRunning = true
+			go GetMACDLoop(botUrl, int64(botMessage.ChatId)) // Запускаем GetMACD в отдельной горутине
+		}
+	} else if update.Message.Text == "/stop" {
+		if isRunning {
+			botMessage.Text = "MACD остановлено."
+			isRunning = false
+		} else {
+			botMessage.Text = "MACD уже остановлено."
+		}
+	} else {
+		botMessage.Text = "Неизвестная команда."
+	}
+
 	buf, err := json.Marshal(botMessage)
 	if err != nil {
 		return err
@@ -31,4 +52,33 @@ func Respond(botUrl string, update models.Update) error {
 		return err
 	}
 	return nil
+}
+
+func GetMACDLoop(botUrl string, chatID int64) {
+	for isRunning {
+		macdValue := GetMACD(client, symbol, interval, limit)
+		var botMessage models.BotMessage
+		if macdValue > 0 {
+			botMessage = models.BotMessage{
+				ChatId: int(chatID),
+				Text:   strconv.FormatFloat(macdValue, 'f', -1, 64) + " 🟢",
+			}
+		} else {
+			botMessage = models.BotMessage{
+				ChatId: int(chatID),
+				Text:   strconv.FormatFloat(macdValue, 'f', -1, 64) + " 🔴",
+			}
+		}
+		buf, err := json.Marshal(botMessage)
+		if err != nil {
+			log.Println("Ошибка при маршалинге сообщения:", err)
+			continue
+		}
+		_, err = http.Post(botUrl+"/sendMessage", "application/json", bytes.NewBuffer(buf))
+		if err != nil {
+			log.Println("Ошибка при отправке сообщения:", err)
+		}
+
+		time.Sleep(time.Second) // Подождать 1 секунду перед следующей проверкой
+	}
 }
