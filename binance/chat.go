1cabf88ed4270de37396aed9d04e5cbc57a124c2
Qazaq159 <zat.tyme.su@gmail.com>
2024-01-30 00:25:11 +0600

    Revert "commit: chat txt reading logic change"
    This reverts commit 8e3ca33bc3d59db28e401126caf9b4dd142fcd2c.

--- binance/chat.go
+++ binance/chat.go
@@ -98,7 +98,7 @@ func removeActiveSession(chatID int64) error {
 }
 
 func ReadLines(filename string) ([]string, error) {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
